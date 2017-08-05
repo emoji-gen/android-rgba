477c9e4e2d695939fd99cfacd72b98d8976c6a37
Pine Mizune <pinemz@gmail.com>
2017-08-05 23:28:54 +0900

    feat(RGBAUtils): add tests

--- rgba/src/androidTest/java/pine/moe/rgba/RGBAUtilsTest.kt
+++ rgba/src/androidTest/java/pine/moe/rgba/RGBAUtilsTest.kt
@@ -1,6 +1,5 @@
 package pine.moe.rgba
 
-
 import android.graphics.Color
 import android.support.test.runner.AndroidJUnit4
 import org.junit.Assert.assertEquals
@@ -8,33 +7,60 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import pine.moe.rgba.RGBAUtils.RGBAToColor
 
-
 @RunWith(AndroidJUnit4::class)
 class RGBAUtilsTest {
     @Test
     fun testRGBAToColor() {
+        assertEquals(Color.argb(0x00, 0x00, 0x00, 0x00), RGBAToColor("0000"))
+        assertEquals(Color.argb(0x11, 0x00, 0x00, 0x00), RGBAToColor("0001"))
+        assertEquals(Color.argb(0x22, 0x00, 0x00, 0x00), RGBAToColor("0002"))
+        assertEquals(Color.argb(0x33, 0x00, 0x00, 0x00), RGBAToColor("0003"))
+        assertEquals(Color.argb(0x44, 0x00, 0x00, 0x00), RGBAToColor("0004"))
+        assertEquals(Color.argb(0x55, 0x00, 0x00, 0x00), RGBAToColor("0005"))
+        assertEquals(Color.argb(0x66, 0x00, 0x00, 0x00), RGBAToColor("0006"))
+        assertEquals(Color.argb(0x77, 0x00, 0x00, 0x00), RGBAToColor("0007"))
+        assertEquals(Color.argb(0x88, 0x00, 0x00, 0x00), RGBAToColor("0008"))
+        assertEquals(Color.argb(0x99, 0x00, 0x00, 0x00), RGBAToColor("0009"))
+        assertEquals(Color.argb(0xaa, 0x00, 0x00, 0x00), RGBAToColor("000a"))
+        assertEquals(Color.argb(0xbb, 0x00, 0x00, 0x00), RGBAToColor("000b"))
+        assertEquals(Color.argb(0xcc, 0x00, 0x00, 0x00), RGBAToColor("000c"))
+        assertEquals(Color.argb(0xdd, 0x00, 0x00, 0x00), RGBAToColor("000d"))
+        assertEquals(Color.argb(0xee, 0x00, 0x00, 0x00), RGBAToColor("000e"))
+        assertEquals(Color.argb(0xff, 0x00, 0x00, 0x00), RGBAToColor("000f"))
+
+        assertEquals(Color.argb(0xaa, 0x00, 0x00, 0x00), RGBAToColor("000A"))
+        assertEquals(Color.argb(0xbb, 0x00, 0x00, 0x00), RGBAToColor("000B"))
+        assertEquals(Color.argb(0xcc, 0x00, 0x00, 0x00), RGBAToColor("000C"))
+        assertEquals(Color.argb(0xdd, 0x00, 0x00, 0x00), RGBAToColor("000D"))
+        assertEquals(Color.argb(0xee, 0x00, 0x00, 0x00), RGBAToColor("000E"))
+        assertEquals(Color.argb(0xff, 0x00, 0x00, 0x00), RGBAToColor("000F"))
+
         assertEquals(Color.argb(0x00, 0x00, 0x00, 0x00), RGBAToColor("00000000"))
         assertEquals(Color.argb(0xff, 0x00, 0x00, 0x00), RGBAToColor("000000ff"))
         assertEquals(Color.argb(0x00, 0xff, 0x00, 0x00), RGBAToColor("ff000000"))
         assertEquals(Color.argb(0x00, 0x00, 0xff, 0x00), RGBAToColor("00ff0000"))
         assertEquals(Color.argb(0x00, 0x00, 0x00, 0xff), RGBAToColor("0000ff00"))
+        assertEquals(Color.argb(0xff, 0xff, 0xff, 0xff), RGBAToColor("ffffffff"))
 
         assertEquals(Color.argb(0x00, 0x00, 0x00, 0x00), RGBAToColor("00000000"))
         assertEquals(Color.argb(0xff, 0x00, 0x00, 0x00), RGBAToColor("000000FF"))
         assertEquals(Color.argb(0x00, 0xff, 0x00, 0x00), RGBAToColor("FF000000"))
         assertEquals(Color.argb(0x00, 0x00, 0xff, 0x00), RGBAToColor("00FF0000"))
         assertEquals(Color.argb(0x00, 0x00, 0x00, 0xff), RGBAToColor("0000FF00"))
+        assertEquals(Color.argb(0xff, 0xff, 0xff, 0xff), RGBAToColor("FFFFFFFF"))
 
         assertEquals(Color.argb(0x00, 0x00, 0x00, 0x00), RGBAToColor("#00000000"))
         assertEquals(Color.argb(0xff, 0x00, 0x00, 0x00), RGBAToColor("#000000ff"))
         assertEquals(Color.argb(0x00, 0xff, 0x00, 0x00), RGBAToColor("#ff000000"))
         assertEquals(Color.argb(0x00, 0x00, 0xff, 0x00), RGBAToColor("#00ff0000"))
         assertEquals(Color.argb(0x00, 0x00, 0x00, 0xff), RGBAToColor("#0000ff00"))
+        assertEquals(Color.argb(0xff, 0xff, 0xff, 0xff), RGBAToColor("#ffffffff"))
 
         assertEquals(Color.argb(0x00, 0x00, 0x00, 0x00), RGBAToColor("#00000000"))
         assertEquals(Color.argb(0xff, 0x00, 0x00, 0x00), RGBAToColor("#000000FF"))
         assertEquals(Color.argb(0x00, 0xff, 0x00, 0x00), RGBAToColor("#FF000000"))
         assertEquals(Color.argb(0x00, 0x00, 0xff, 0x00), RGBAToColor("#00FF0000"))
         assertEquals(Color.argb(0x00, 0x00, 0x00, 0xff), RGBAToColor("#0000FF00"))
+        assertEquals(Color.argb(0xff, 0xff, 0xff, 0xff), RGBAToColor("#FFFFFFFF"))
     }
 }
--- rgba/src/main/java/pine/moe/rgba/RGBAUtils.java
+++ rgba/src/main/java/pine/moe/rgba/RGBAUtils.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.annotation.NonNull;
 
 /**
+ * Utilities for RGBA
  * Created by pine on 2017/08/04.
  */
 
@@ -14,13 +15,20 @@ public class RGBAUtils {
         }
 
         int pos = rgba.charAt(0) == '#' ? 1 : 0;
+
         if (rgba.length() - pos == 4) {
             int red = Integer.valueOf(rgba.substring(pos, pos + 1), 16);
             int green = Integer.valueOf(rgba.substring(pos + 1, pos + 2), 16);
             int blue = Integer.valueOf(rgba.substring(pos + 2, pos + 3), 16);
             int alpha = Integer.valueOf(rgba.substring(pos + 3, pos + 4), 16);
-            return Color.argb(alpha, red, green, blue);
+            return Color.argb(
+                    alpha << 4 | alpha,
+                    red << 4 | red,
+                    green << 4 | green,
+                    blue << 4 | blue
+            );
         }
+
         if (rgba.length() - pos == 8) {
             int red = Integer.valueOf(rgba.substring(pos, pos + 2), 16);
             int green = Integer.valueOf(rgba.substring(pos + 2, pos + 4), 16);
